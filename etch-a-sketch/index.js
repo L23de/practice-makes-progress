7211296771bbaefe269eb3fec748f237f75f716e
Lester Huang <huangl302d@gmail.com>
2022-01-04 22:06:26 -0500

    incorporate local storage of html state

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -33,6 +33,10 @@ function colorIn() {
         let color = document.getElementById('color-picker').value
         colorChoice.call(this, [color]);
     }
+    // Saves local state to local storage, not the most efficient, since it saves entire HTML document
+    // But with such a simple program, there does not seem to be a good interval to save the document in between coloring
+    // Even with constant saving, performance tab on Chrome looks reasonable
+    localStorage.setItem('saveData', document.documentElement.innerHTML); 
 }
 
 function colorChoice(bgColor) {
@@ -49,11 +53,21 @@ function colorRGB() {
 // On load (Default behavior)
 let truthy = false; // Global var modified by sliding switch
 
-createGrid(20);
+var prevData = localStorage.getItem('saveData') // Retrieve data from local storage if exists
+if (prevData) {
+    document.documentElement.innerHTML = prevData
+} else {
+    createGrid(20); // Create default grid if fresh start
+}
+
+var boxes = document.getElementsByClassName('grid-box');
+Array.from(boxes).forEach((element) => {
+    element.addEventListener('mouseover', colorIn);
+})
+
 document.getElementById('submit').addEventListener('click', resetGrid);
 document.getElementById('switch-box').addEventListener('change', () => truthy = !truthy);
 document.getElementById('color-picker').addEventListener('change', (event) => {
-    console.log(event.target.value)
     document.getElementById('switch-slider').style.backgroundColor = event.target.value
 })
 
