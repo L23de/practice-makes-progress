39b7eefa81cbff762cd3e02f3e927cf3f0dddae6
Lester Huang <huangl302d@gmail.com>
2022-01-04 20:23:49 -0500

    basic etch-a-sketch works

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,3 +1,4 @@
+// Grid functions
 function createGrid(length) {
     const mainGrid = document.querySelector('#grid');
     mainGrid.innerHTML = ''; // Clears previous grid contents
@@ -21,11 +22,35 @@ function resetGrid() {
     createGrid(parseInt(newLength));
 }
 
-function addColor() {
 
+// SkEtching functions
+function colorIn() {
+    // Applies the other color functions on 'this' context
+    // colorRGB.call(this);
+    colorBlack.call(this);
 }
 
+function colorBlack() {
+    this.classList.toggle('black-box');
+}
+
+function colorRGB(event) {
+    // Source for random color: https://css-tricks.com/snippets/javascript/random-hex-color/
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    this.style.backgroundColor = `#${randomColor}`;
+}
+
+
+// On load (Default behavior)
+createGrid(20);
+
+// Set up event listeners
 document.getElementById('submit').addEventListener('click', resetGrid);
-// document.getElementsByClassName('grid-box').array.forEach(element => { element.addEventListener('hover', addColor)
-// });
-createGrid(16); // Default page load
+
+var boxes = document.getElementsByClassName('grid-box');
+Array.from(boxes).forEach((box) => {
+    box.addEventListener('mouseover', colorIn)
+});
+
+
+
